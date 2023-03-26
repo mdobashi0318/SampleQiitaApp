197d155cda280188a0bde3048434df76c84be179
MasaharuDobashi <masaharu.dobashi@gmail.com>
2023-03-27 01:21:53 +0900

    ProgressBar追加

--- app/src/main/java/com/example/sampleqiitaapp/ArticleListFragment.kt
+++ app/src/main/java/com/example/sampleqiitaapp/ArticleListFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import androidx.fragment.app.Fragment
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.sampleqiitaapp.adapter.ArticleAdapter
@@ -26,18 +27,34 @@ class ArticleListFragment : Fragment() {
         val itemDecoration = DividerItemDecoration(context, DividerItemDecoration.VERTICAL)
         binding.recyclerView.addItemDecoration(itemDecoration)
 
+        visibleProgressBar()
         APIManager.get<Article>("items", {
             val adapter = ArticleAdapter(it)
             binding.recyclerView.adapter = adapter
+            goneProgressBar()
         }) {
             // TODO: 通信失敗のダイアログを出す
+            goneProgressBar()
         }
-
-
-
 
         return binding.root
     }
 
+
+    /**
+    ProgressBarを表示させ、画面操作を無効にする
+     **/
+    private fun visibleProgressBar() {
+        binding.progressBar.visibility = View.VISIBLE
+        activity?.window?.addFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE)
+    }
+
+    /**
+    ProgressBarを消し、画面操作を有効にする
+     **/
+    private fun goneProgressBar() {
+        binding.progressBar.visibility = View.GONE
+        activity?.window?.clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE)
+    }
 
 }
