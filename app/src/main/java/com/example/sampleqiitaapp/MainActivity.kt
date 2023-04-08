7e8c621d3b5b934afd13ce7d72d193e18d7f4396
MasaharuDobashi <masaharu.dobashi@gmail.com>
2023-04-08 17:23:08 +0900

    テーマとアクションバーの更新

--- app/src/main/java/com/example/sampleqiitaapp/MainActivity.kt
+++ app/src/main/java/com/example/sampleqiitaapp/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.sampleqiitaapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.sampleqiitaapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+
+    private val binding by lazy {
+        DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setSupportActionBar(binding.toolbar)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
     }
+
+    override fun onSupportNavigateUp() = findNavController(R.id.nav_host_fragment).navigateUp()
+
 }
